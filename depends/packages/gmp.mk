bc0d981f4920335b926bff5528f91deb696f2aa7
ColibriCrypto <95352289+ColibriCrypto@users.noreply.github.com>
2021-12-07 17:58:27 +0200

    Add files via upload

---
+++ depends/packages/gmp.mk
@@ -0,0 +1,25 @@
+package= clbi
+$(package)_version=6.2.1
+$(package)_download_path=https:// clbilib.org/download/ clbi
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
+
+define $(package)_set_vars
+$(package)_config_opts=--disable-shared
+$(package)_config_opts_mingw32=--enable-mingw
+$(package)_config_opts_linux=--with-pic
+$(package)_config_opts_aarch64_darwin+=--with-pic
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
